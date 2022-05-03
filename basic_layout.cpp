934553272465a5938ea16f59aeab461adbe53a11
shubham kumar <18shubham1998@gmail.com>
2022-05-04 00:53:22 +0530

    basic_layout

---
+++ basic_layout.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    ofstream coutf("outputtxt.txt");
+    ifstream cinf("inputtxt.txt");
+    vector<int> v;
+    if (cinf.is_open())
+    {
+        {
+            int i;
+            while (cinf >> i)
+            {
+                v.push_back(i);
+            }
+            cinf.close();
+        }
+    }
+    else
+    {
+        cout << "Unable to open file";
+    }
+    for (int i = 0; i < v.size(); i++)
+    {
+        coutf << v[i] << endl;
+    }
+}
