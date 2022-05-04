3c01ddc55ecf48578c8a136d08eccc65db348e84
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-05-04 21:16:17 +0530

    Infernos

--- basic_layout.cpp
+++ basic_layout.cpp
@@ -1,13 +1,20 @@
 /*
-Chef wants to buy a new laptop. However, he is confused about which laptop to buy out of 10 different laptops. He asks his N friends for their recommendation. The ith friend recommends the Chef to buy the Aithlaptop (1≤Ai≤10).
+Infernos Problem Code: INFERNO
+Add problem to Todo list
+Submit
+Ved started playing a new mobile game called Fighting Clans. An army of N enemies is approaching his base. The ith enemy has Hi health points. An enemy gets killed if his health points become 0.
+Ved defends his base using a weapon named Inferno. He can set the Inferno to one of the following two modes:
 
-Chef will buy the laptop which is recommended by maximum number of friends. Determine which laptop Chef buys.
-Print CONFUSED if there are multiple laptops having maximum number of recommendations.
+Single-Target Mode: In one second, the Inferno can target exactly one living enemy and cause damage of at most X health points.
+Multi-Target Mode: In one second, the Inferno can target all living enemies and cause damage of 1 health point to each of them.
+Find the minimum time required to kill all the enemies.
+
+Note: Ved is not allowed to change the mode of the weapon once it is set initially.
 
 Input Format
 The first line contains a single integer T - the number of test cases. Then the test cases follow.
-The first line of each test case contains an integer N - the number of Chef's friends.
-The second line of each test case contains N space-separated integers A1,A2,…,AN where Ai denotes the recommendation of the ith friend.
+The first line of each test case contains two integers N and X - the number of enemies and the damage caused by the single-target mode of Inferno in one second respectively.
+The second line of each test case contains N space-separated integers H1,H2,…,HN where Hi denotes the initial health points of ith enemy..
 */
 #include <iostream>
 #include <map>
@@ -20,46 +27,7 @@ int main()
     ifstream cinf("inputtxt.txt");
     if (cinf.is_open())
     {
-        int count;
-        cinf >> count;
-        while (count--)
-        {
-            int siz;
-            cinf >> siz;
-            map<int, int> m;
-            for (int i = 0; i < siz; i++)
-            {
-                int temp;
-                cinf >> temp;
-                m[temp]++;
-            }
-            int max = 0;
-            int max_key1 = 0;
-            int no_of_max = 0;
-            for (auto it = m.begin(); it != m.end(); it++)
-            {
-                if (it->second > max)
-                {
-                    max = it->second;
-                    max_key1 = it->first;
-                }
-            }
-            for (auto it = m.begin(); it != m.end(); it++)
-            {
-                if (it->second == max)
-                {
-                    no_of_max++;
-                }
-            }
-            if (no_of_max > 1)
-            {
-                coutf << "CONFUSED" << endl;
-            }
-            else
-            {
-                coutf << max_key1 << endl;
-            }
-        }
+        
     }
     else
     {
