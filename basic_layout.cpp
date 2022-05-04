75aeb51eddc1f1208da70018fb87c00fc0963d5d
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-05-04 22:20:09 +0530

    Beat the Average

--- basic_layout.cpp
+++ basic_layout.cpp
@@ -1,39 +1,20 @@
 /*
-Infernos Problem Code: INFERNO
+Beat the Average Problem Code: ABOVEAVG
 Add problem to Todo list
 Submit
-Ved started playing a new mobile game called Fighting Clans. An army of N enemies is approaching his base. The ith enemy has Hi health points. An enemy gets killed if his health points become 0.
-Ved defends his base using a weapon named Inferno. He can set the Inferno to one of the following two modes:
+There are N students in a class. Recently, an exam on Advanced Algorithms was conducted with maximum score M and minimum score 0. The average score of the class was found out to be exactly X.
 
-Single-Target Mode: In one second, the Inferno can target exactly one living enemy and cause damage of at most X health points.
-Multi-Target Mode: In one second, the Inferno can target all living enemies and cause damage of 1 health point to each of them.
-Find the minimum time required to kill all the enemies.
-
-Note: Ved is not allowed to change the mode of the weapon once it is set initially.
+Given that a student having score strictly greater than the average receives an A grade, find the maximum number of students that can receive an A grade.
 
 Input Format
-The first line contains a single integer T - the number of test cases. Then the test cases follow.
-The first line of each test case contains two integers N and X - the number of enemies and the damage caused by the single-target mode of Inferno in one second respectively.
-The second line of each test case contains N space-separated integers H1,H2,…,HN where Hi denotes the initial health points of ith enemy..
+First line will contain T, number of test cases. Then the test cases follow.
+The only line of each test case consists of three integers N,M,X - the number of students, the maximum score and the average score respectively.
 */
 #include <iostream>
 #include <vector>
 #include <numeric>
 #include <fstream>
 using namespace std;
-bool for_each(vector<int>::iterator beg, vector<int>::iterator end)
-{
-    bool test = true;
-    for (; beg != end; ++beg)
-    {
-        if (*beg > 0)
-        {
-            test = false;
-            break;
-        }
-    }
-    return test;
-}
 
 int main()
 {
@@ -43,44 +24,6 @@ int main()
     if (cinf.is_open())
     {
 
-        int t;
-        cinf >> t;
-        while (t--)
-        {
-            int n, x;
-            cinf >> n >> x;
-            vector<int> h(n);
-            for (auto &i : h)
-            {
-                cinf >> i;
-            }
-            int max = 0;
-            for (auto i : h)
-            {
-                if (i > max)
-                    max = i;
-            }
-            int tim = 0;
-            while (!for_each(h.begin(), h.end()))
-            {
-                for (auto &i : h)
-                {
-                    if (i > 0)
-                    {
-                        i -= x;
-                        tim++;
-                    }
-                }
-            }
-            if (max > tim)
-            {
-                coutf << tim << endl;
-            }
-            else
-            {
-                coutf << max << endl;
-            }
-        }
     }
     else
     {
