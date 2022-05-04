004d2825f6ddc21de8f60754ba4c92751f967d30
shubham kumar <41205124+Shub1970@users.noreply.github.com>
2022-05-04 20:11:00 +0530

    Update basic_layout.cpp

--- basic_layout.cpp
+++ basic_layout.cpp
@@ -1,4 +1,14 @@
+/*
+Roller Coaster Problem Code: MINHEIGHT
+Add problem to Todo list
+Submit
+Chef's son wants to go on a roller coaster ride. The height of Chef's son is X inches while the minimum height required to go on the ride is H inches. Determine whether he can go on the ride or not.
 
+Input Format
+The first line contains a single integer T - the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two integers X and H - the height of Chef's son and the minimum height required for the ride respectively.
+
+*/
 #include <iostream>
 #include <vector>
 #include <fstream>
