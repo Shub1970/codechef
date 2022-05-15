f3431bfd8af4b774b9be2f8a38cca8d640511a97
shubham kumar <18shubham1998@gmail.com>
2022-05-15 19:57:47 +0530

    solve lunch time

--- basic_layout.cpp
+++ basic_layout.cpp
@@ -17,46 +17,7 @@ int main()
         cinf >> test;
         while (test--)
         {
-            int length;
-            cinf >> length;
-            vector<int> A;
-            vector<int> B;
-            int ans = 0;
-            for (int i = 0; i < length; i++)
-            {
-                int temp;
-                cinf >> temp;
-                A.push_back(temp);
-            }
-            for (int i = 0; i < length; i++)
-            {
-                int temp;
-                cinf >> temp;
-                B.push_back(temp);
-            }
-            unordered_map<int, vector<int>> stairs;
-            for (int i = 0; i < length; i++)
-            {
-                stairs[A[i]].push_back(i);
-            }
-            for (int i = 0; i < length; i++)
-            {
-                if (stairs[B[i]].size() > 0)
-                {
-                    for (auto r : stairs[B[i]])
-                    {
-                        if (r != i)
-                        {
-                            ans++;
-                        }
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-            }
-            coutf << ans / 2 << endl;
+            
         }
     }
     else
