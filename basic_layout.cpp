4b25bcda5bc0c8e388dc7c9ff5aa2c4f2a272584
shubham kumar <18shubham1998@gmail.com>
2022-05-18 22:04:59 +0530

    solve fifth

--- basic_layout.cpp
+++ basic_layout.cpp
@@ -1,8 +1,36 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 using namespace std;
+pair<int, int> range(char a)
+{
+    int left;
+    int right;
+    char range[5]{'a', 'e', 'i', 'o', 'u'};
+    if (a < 'v')
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 1; j <= 4; j++)
+            {
+                if (range[i] <= a && a <= range[j])
+                {
+                    left = static_cast<int>(a - range[i]);
+                    right = static_cast<int>(range[j] - a);
+                    break;
+                }
+            }
+        }
+    }
+    else
+    {
+        left = a - 'u';
+        right = 130 - a;
+    }
+    return make_pair(left, right);
+}
 int main()
 {
     ofstream coutf("outputtxt.txt");
@@ -14,30 +42,22 @@ int main()
         cinf >> test;
         while (test--)
         {
-            int no_frame;
-            cinf >> no_frame;
-            vector<int> frame;
-            while (no_frame--)
+            int size;
+            cinf >> size;
+            string st;
+            cinf >> st;
+            long long int n = 1;
+            for (auto x : st)
             {
-                int temp;
-                cinf >> temp;
-                frame.push_back(temp);
-            }
-            int min_frame = 0;
-            int repeat_frame = 0;
-            for (auto x : frame)
-            {
-                if (x != repeat_frame)
+                if (x != 'a' || x != 'e' || x != 'i' || x != 'o' || x != 'u')
                 {
-                    repeat_frame = x;
-                    min_frame++;
-                }
-                else
-                {
-                    continue;
+                    if (range(x).first == range(x).second)
+                    {
+                        n = n * 2;
+                    }
                 }
             }
-            coutf << min_frame << endl;
+            return n % (1000000007);
         }
     }
     else
